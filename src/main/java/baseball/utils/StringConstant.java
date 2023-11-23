35b8e7f00cc89179e421b9989430931acafcf0c3
Jin Geonwoo <jinkonu99@gmail.com>
2023-11-23 17:07:08 +0900

    feat: OutputView 추가

--- src/main/java/baseball/utils/StringConstant.java
+++ src/main/java/baseball/utils/StringConstant.java
@@ -7,11 +7,18 @@ public class StringConstant {
     /* RESULT */
     public static final String STRIKE_UNIT = "스트라이크";
     public static final String BALL_UNIT = "볼";
-    public static final String NOTHING_UNIT = "낫싱";
+    public static final String NOTHING = "낫싱";
 
 
     /* INPUT */
     public static final char CHAR_ZERO = '0';
+
+
+    /* OUTPUT */
+    public static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    public static final String GUESS_INPUT_FORM_MESSAGE = "숫자를 입력해주세요 : ";
+    public static final String SUCCESS_MESSAGE = SUCCESS_NUM + "개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String RESTART_MESSAGE = "게임을 새로 시작하려면 "+ REPLAY_NUM + ", 종료하려면 "+ QUIT_NUM + "를 입력하세요.";
 
 
     /* ERROR */
---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,35 @@
+package baseball.view;
+
+import static baseball.utils.StringConstant.*;
+
+public class OutputView {
+
+    public static void printStartApplication() {
+        System.out.println(START_MESSAGE);
+    }
+
+    public static void printGuessInputForm() {
+        System.out.print(GUESS_INPUT_FORM_MESSAGE);
+    }
+
+    public static void printResult(String unit, int size) {
+        System.out.println(size + unit);
+    }
+
+    public static void printNothing() {
+        System.out.println(NOTHING);
+    }
+
+    public static void printSuccess() {
+        System.out.println(SUCCESS_MESSAGE);
+    }
+
+    public static void printRestart() {
+        System.out.println(RESTART_MESSAGE);
+    }
+
+
+    public static void printError(Exception e) {
+        System.out.println(ERROR_PREFIX + e.getMessage());
+    }
+}
