4ba137a2ff3a3c676f945c21c0400b6fb3a8ad48
Jin Geonwoo <jinkonu99@gmail.com>
2023-11-23 15:08:35 +0900

    feat: 도메인 로직 정의

--- src/main/java/baseball/domain/Answer.java
+++ src/main/java/baseball/domain/Answer.java
@@ -2,17 +2,35 @@ package baseball.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.stream.IntStream;
 
+import static baseball.domain.Result.*;
 import static baseball.utils.NumberConstant.*;
 
 public class Answer {
+
     private final List<Integer> numbers;
 
     public Answer() {
         numbers = Randoms.pickUniqueNumbersInRange(START_NUM, END_NUM, NUMBERS_SIZE);
+    }
+
+    public List<Result> matches(int[] guessNumbers) {
+        List<Result> results = new ArrayList<>();
+
+        IntStream.range(ZERO, NUMBERS_SIZE).forEach(i -> {
+            results.add(matchOneByOne(numbers.get(i), guessNumbers[i], i));
+        });
+
+        return results;
+    }
+
+    private Result matchOneByOne(int answer, int guess, int idx) {
+        if (answer == guess) return STRIKE;
+        if (numbers.contains(guess)) return BALL;
+
+        return NOTHING;
     }
 
     @Override
--- src/main/java/baseball/domain/Result.java
+++ src/main/java/baseball/domain/Result.java
@@ -3,9 +3,12 @@ package baseball.domain;
 import static baseball.utils.StringConstant.*;
 
 public enum Result {
+
     STRIKE(STRIKE_UNIT),
     BALL(BALL_UNIT),
-    NOTHING(NOTHING_UNIT);
+    NOTHING(NOTHING_UNIT),
+    ;
 
-    Result(String message) { }
+    Result(String message) {
+    }
 }
